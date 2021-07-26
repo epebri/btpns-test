8cbb45371ea58991f6f72c7ddd651b2c8459f398
Eko Pebrisulistiyo <eko.pebrisulistiyo@gmail.com>
2021-07-26 23:58:39 +0800

    Update Response Menu API

--- src/main/java/com/mitrais/pos/controller/MenuController.java
+++ src/main/java/com/mitrais/pos/controller/MenuController.java
@@ -1,6 +1,7 @@
 package com.mitrais.pos.controller;
 
 import com.mitrais.pos.entity.Menu;
+import com.mitrais.pos.model.response.ResponseHandler;
 import com.mitrais.pos.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,13 +21,19 @@ public class MenuController {
     MenuService menuService;
 
     @GetMapping("/menus")
-    public ResponseEntity<List<Menu>> getAllMenus(@RequestParam(required = false) String type, @RequestParam(required = false) String category) {
-           try {
-               List<Menu> menus = menuService.showMenu(type, category);
-               return new ResponseEntity<>(menus, HttpStatus.OK);
-           } catch (Exception e) {
-               e.printStackTrace();
-               return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-           }
+    public ResponseEntity<ResponseHandler> getAllMenus(@RequestParam(required = false) String type, @RequestParam(required = false) String category) {
+        ResponseHandler response = new ResponseHandler();
+        try {
+            List<Menu> menus = menuService.showMenu(type, category);
+            response.setCode(200L);
+            response.setMessage("Get Data menu success!");
+            response.setData(menus);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setCode(500L);
+            response.setMessage(e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
